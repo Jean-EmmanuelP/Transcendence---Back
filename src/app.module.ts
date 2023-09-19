3f71cb563915e97631f92a940762de538dd5cde7
Jean-emmanuel PERRAMANT <jperrama@made-f0Dr7s7.clusters.42paris.fr>
2023-09-19 15:50:44 +0200

    new commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      installSubscriptionHandlers: true,
+    }),
+  ],
+})
+export class AppModule {}
