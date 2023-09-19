7babc5289a428bf01fd4d117bb570727a3fc7055
Perramant <perramant.work@gmail.com>
2023-09-19 20:41:35 +0200

    checkin

---
+++ dist/app.module.js
@@ -0,0 +1,33 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppModule = void 0;
+const apollo_1 = require("@nestjs/apollo");
+const common_1 = require("@nestjs/common");
+const graphql_1 = require("@nestjs/graphql");
+const user_controller_1 = require("./user/controllers/user/user.controller");
+const user_service_1 = require("./user/services/user/user.service");
+const user_module_1 = require("./user/user.module");
+let AppModule = class AppModule {
+};
+exports.AppModule = AppModule;
+exports.AppModule = AppModule = __decorate([
+    (0, common_1.Module)({
+        imports: [
+            graphql_1.GraphQLModule.forRoot({
+                driver: apollo_1.ApolloDriver,
+                autoSchemaFile: 'schema.gql',
+                installSubscriptionHandlers: true,
+            }),
+            user_module_1.UserModule,
+        ],
+        controllers: [user_controller_1.UserController],
+        providers: [user_service_1.UserService],
+    })
+], AppModule);
+//# sourceMappingURL=app.module.js.map
