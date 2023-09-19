f8fc89c250be190487d7cc27eaab92859827cd32
Perramant <perramant.work@gmail.com>
2023-09-19 16:20:42 +0200

    doing user module architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { UserController } from './user/controllers/user/user.controller';
+import { UserService } from './user/services/user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -9,6 +12,9 @@ import { GraphQLModule } from '@nestjs/graphql';
       autoSchemaFile: 'schema.gql',
       installSubscriptionHandlers: true,
     }),
+    UserModule,
   ],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
