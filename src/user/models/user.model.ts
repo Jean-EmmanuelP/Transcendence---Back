73a6cbcb5546a6f1c0f8116a52a0a8a932f05014
unknown <perramant.work@gmail.com>
2023-09-20 00:04:26 +0200

    usermodel for the resolver in args and the return also creating the UserCreateInput dto

--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -1,7 +1,7 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
-export class UserType {
+export class UserModel {
   @Field(() => Int)
   id: number;
 
--- src/user/resolvers/user/user.resolver.ts
+++ src/user/resolvers/user/user.resolver.ts
@@ -1,19 +1,19 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { UserService } from './../../services/user/user.service';
-import { User, Prisma } from '@prisma/client'
-import { UserType } from 'src/user/models/user.model';
+import { Prisma } from '@prisma/client'
+import { UserModel } from 'src/user/models/user.model';
 
-@Resolver('User')
+@Resolver(of => UserModel)
 export class UserResolver {
     constructor(private readonly userService: UserService) {}
 
-    @Query(() => [UserType])
-    async users(): Promise<User[]> {
+    @Query(() => [UserModel])
+    async users(): Promise<UserModel[]> {
         return this.userService.findAll();
     }
 
-    @Mutation(() => UserType)
-    async createUser(@Args('data') data: Prisma.UserCreateInput): Promise<User> {
+    @Mutation(() => UserModel)
+    async createUser(@Args('data') data: Prisma.UserCreateInput): Promise<UserModel> {
         return this.userService.create(data);
     }
 
