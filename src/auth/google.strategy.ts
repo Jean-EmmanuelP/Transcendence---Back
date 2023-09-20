203aa3fcb7c68f55d334d791178c7e1274f74982
Perramant <perramant.work@gmail.com>
2023-09-20 15:00:22 +0200

    added the google env

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -6,8 +6,8 @@ import { Strategy } from 'passport-google-oauth20';
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     constructor() {
         super({
-            clientID: 'YOUR_GOOGLE_CLIENT_ID',
-            clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: 'http://localhost:3000/auth/google/callback',
             scope: ['email', 'profile'],
         });
