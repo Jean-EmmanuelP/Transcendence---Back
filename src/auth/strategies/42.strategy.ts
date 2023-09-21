eabd7e313e7bafa1cc5ab8b7563c78f554079262
Perramant <perramant.work@gmail.com>
2023-09-21 18:30:50 +0200

    done the google auth but now doing the 42 strategy struggling with some failed to fetch usr profile

--- src/auth/strategies/42.strategy.ts
+++ src/auth/strategies/42.strategy.ts
@@ -1,30 +1,35 @@
 // strategies/42.strategy.ts
 
-import { Injectable } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy as FortyTwoStrategy } from 'passport-42';
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy as FortyTwoStrategy } from "passport-42";
 
 @Injectable()
-export class FortyTwoAuthStrategy extends PassportStrategy(FortyTwoStrategy, '42') {
-    constructor() {
-        super({
-            clientID: process.env.FORTYTWO_CLIENT_ID,
-            clientSecret: process.env.FORTYTWO_CLIENT_SECRET,
-            callbackURL: 'http://localhost:3000/auth/42-redirect',
-        });
-    }
+export class FortyTwoAuthStrategy extends PassportStrategy(
+  FortyTwoStrategy,
+  "42"
+) {
+  constructor() {
+    super({
+      clientID: process.env.FORTYTWO_CLIENT_ID,
+      clientSecret: process.env.FORTYTWO_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/42-redirect",
+    });
+  }
 
-    async validate(accessToken: string, refreshToken: string, profile: any): Promise<any> {
-        // Le profil contient désormais les informations de l'utilisateur retournées par 42.
-        // Ici, vous pouvez décider de ce que vous voulez faire avec ces informations,
-        // comme les stocker dans votre base de données, etc.
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any
+  ): Promise<any> {
+    // Le profil contient désormais les informations de l'utilisateur retournées par 42.
+    // Ici, vous pouvez décider de ce que vous voulez faire avec ces informations,
+    // comme les stocker dans votre base de données, etc.
 
-        // Pour l'instant, retournons simplement l'utilisateur.
-        return {
-            userId: profile.id,
-            username: profile.login,
-            email: profile.email,
-            // ... ajoutez d'autres champs si nécessaire
-        };
-    }
+    // Pour l'instant, retournons simplement l'utilisateur.
+    console.log(`You went here`);
+    return {
+      profile
+    };
+  }
 }
