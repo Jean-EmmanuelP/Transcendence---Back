5aac74ec9fb93ad96dc2c844da9406d6ffe10054
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-09-29 18:38:00 +0200

    upgraded the jwt guard with more error handling

--- src/guards/jwt.guard.ts
+++ src/guards/jwt.guard.ts
@@ -2,6 +2,7 @@ import { ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/com
 import { AuthGuard } from "@nestjs/passport";
 import { GqlExecutionContext } from "@nestjs/graphql";
 import { JwtService } from "@nestjs/jwt";
+import { TokenExpiredError } from "jsonwebtoken";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -14,14 +15,20 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     const request = ctx.getContext().req;
     console.log(`from the canactivate`, request.headers.authorization);  
     const token = request.headers.authorization?.split(' ')[1];
-    const { isTemporary } = this.jwtService.verify(token, {
-      secret: process.env.JWT_SECRET,
-    });
-
-    if (isTemporary && request.path !== '/auth/validate-two-factor') {
-      throw new UnauthorizedException('Invalid token');
+    try {
+      const { isTemporary } = this.jwtService.verify(token, {
+        secret: process.env.JWT_SECRET,
+      });
+      if (isTemporary && request.path !== '/auth/validate-two-factor') {
+        throw new UnauthorizedException('Invalid token');
+      }
+    } catch(error) {
+      if (error instanceof TokenExpiredError) {
+        throw new UnauthorizedException('Your session has expired. Please log in again.');
+      } else {
+        throw new UnauthorizedException(`Invalid token ${error.message}`)
+      }
     }
-
     return super.canActivate(context);
   }
 
