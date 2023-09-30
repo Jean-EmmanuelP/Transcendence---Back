78f01c854fae585ef4704672676f98d8bd8c918c
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-09-30 15:16:38 +0200

    added the updatemessage and delete message in ther resolver

--- src/chat/resolvers/chat/chat.resolver.ts
+++ src/chat/resolvers/chat/chat.resolver.ts
@@ -1,6 +1,7 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { ChatService } from 'src/chat/services/chat/chat.service';
-import { CreateDirectChannelOutput, SendMessageInput, SendMessageOutput, createDirectChannelInput } from 'src/chat/services/chat/dtos/channel-dtos';
+import { CreateDirectChannelOutput, DeleteMessageInput, DeleteMessageOutput, SendMessageInput, SendMessageOutput, UpdateMessageOutput, createDirectChannelInput } from 'src/chat/services/chat/dtos/channel-dtos';
+import { UpdateMessageOutput } from './../../services/chat/dtos/channel-dtos';
 
 @Resolver()
 export class ChatResolver {
@@ -16,4 +17,15 @@ export class ChatResolver {
     async sendMessage(@Args('input') input: SendMessageInput): Promise<SendMessageOutput> {
         return this.chatService.sendMessage(input);
     }
+
+    @Mutation(() => UpdateMessageOutput)
+    async updateMessage(@Args('input') input: UpdateMessageOutput): Promise<UpdateMessageOutput> {
+        return this.chatService.updateMessage(input);
+    }
+
+    @Mutation(() => DeleteMessageOutput)
+    async deleteMessage(@Args('input') input: DeleteMessageInput): Promise<DeleteMessageOutput> {
+        return this.chatService.deleteMessage(input);
+    }
+
 }
