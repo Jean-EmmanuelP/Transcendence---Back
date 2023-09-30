f0273db8abd64165585f9594400a68e59c32feb4
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-09-30 10:45:18 +0200

    added the createdirectchannel in the chat.service

--- src/chat/services/chat/chat.service.ts
+++ src/chat/services/chat/chat.service.ts
@@ -1,6 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "prisma/services/prisma/prisma.service";
+import { UserService } from "src/user/services/user/user.service";
 
 @Injectable()
 export class ChatService {
-    
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly userService: UserService
+  ) {}
+
+  async createDirectChannel(userId: string, userId2: string): Promise<void> {
+    const user1 = await this.userService.findById(userId);
+    const user2 = await this.userService.findById(userId2);
+    await this.prisma.channel.create({
+      data: {
+        name: `Direct-${user1.name}-${user2.name}`,
+        isPrivate: true,
+        isDirectMessage: true,
+        members: {
+          connect: [{ id: userId }, { id: userId2 }],
+        },
+      },
+    });
+  }
 }
