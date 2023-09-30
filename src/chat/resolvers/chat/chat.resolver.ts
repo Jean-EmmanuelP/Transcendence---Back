b0a8fc53483bace5d53e9651b6f100df742ddaff
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-09-30 15:19:06 +0200

    added the getUsersChannel

--- src/chat/resolvers/chat/chat.resolver.ts
+++ src/chat/resolvers/chat/chat.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { ChatService } from 'src/chat/services/chat/chat.service';
 import { CreateDirectChannelOutput, DeleteMessageInput, DeleteMessageOutput, GetMessageInput, GetMessageOutput, SendMessageInput, SendMessageOutput, UpdateMessageOutput, createDirectChannelInput } from 'src/chat/services/chat/dtos/channel-dtos';
 import { UpdateMessageOutput } from './../../services/chat/dtos/channel-dtos';
+import { ChannelModel } from 'src/chat/services/chat/models/channel.model';
 
 @Resolver()
 export class ChatResolver {
@@ -31,5 +32,10 @@ export class ChatResolver {
     @Query(() => GetMessageOutput)
     async getMessages(@Args('input') input: GetMessageInput): Promise<GetMessageOutput> {
         return this.chatService.getMessages(input);
+    }
+
+    @Query(() => [ChannelModel])
+    async getUsersChannel(@Args('userId') userId: string): Promise<ChannelModel[]> {
+        return this.chatService.getUserChannels(userId);
     }
 }
