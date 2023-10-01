6bf0a77cbfda71df475fd880abb6617019a62dbd
Perramant <perramant.work@gmail.com>
2023-10-01 19:51:13 +0200

    check the return of the decorator

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -4,6 +4,7 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 export const User = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context);
+    // console.log(ctx.getContext().req.user.userId)
     return ctx.getContext().req.user.userId;
   },
 );
